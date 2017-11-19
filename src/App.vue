a85cebed4bf54af11548935312099f0c1cddeb8c
Rafael Belo <rafaelrpbelo@gmail.com>
2017-11-19 16:57:32 -0300

    Add progress bar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
   <div id='app' class='container'>
+    <div class='row'>
+      <div class='col'>
+        <progress-bar :min=0 :max='maxQuotes' :value='quotes.length'></progress-bar>
+      </div>
+    </div>
+
     <quote-creator @quoteCreated='addNewQuote'></quote-creator>
     <quote-grid :quotes='quotes' @deletedQuote='deleteQuote'></quote-grid>
+
+    <div class='row' v-show='quotes.length > 0'>
+      <div class='col alert alert-primary' role='alert'>
+        Click on quote to delete it!
+      </div>
+    </div>
+    <div class='row' v-show='quotes.length > 0'>
+      <div class='col alert alert-primary' role='alert'>
+        Click on quote to delete it!
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import QuoteGrid from './components/QuoteGrid.vue';
   import QuoteCreator from './components/handlers/QuoteCreator.vue';
+  import ProgressBar from './components/common/ProgressBar.vue';
 
   export default {
     data() {
       return {
-        quotes: ['My first quote']
+        quotes: ['My first quote'],
+        maxQuotes: 10
       }
     },
     methods: {
@@ -25,7 +44,8 @@
     },
     components: {
       quoteGrid: QuoteGrid,
-      quoteCreator: QuoteCreator
+      quoteCreator: QuoteCreator,
+      progressBar: ProgressBar
     }
   }
 </script>
---
+++ src/components/common/ProgressBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="progress">
+    <div
+      class="progress-bar"
+      role="progressbar"
+      :aria-valuenow="value"
+      :aria-valuemin="min"
+      :aria-valuemax="max"
+      :style="getProgressAsStyle()"
+      >{{ text() }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      min: {type: Number, default: 0},
+      max: {type: Number, default: 100},
+      value: {type: Number, default: 0}
+    },
+    methods: {
+      text() {
+        if (this.value > 0) return `${this.value} / ${this.max}`;
+      },
+      getProgressAsStyle() {
+        return { width: `${(this.value / this.max) * 100}%` }
+      }
+    }
+  }
+</script>
